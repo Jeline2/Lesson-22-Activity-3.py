3c0ca566bb530e70e92822b8c9383ff291b61243
Jeline2 <jelineshyju5@gmail.com>
2025-10-07 22:02:47 +0530

    first commit

---
+++ main.py
@@ -0,0 +1,10 @@
+def romanIoInt(romanInput):
+    roman={'M':1000,'D':500, 'C':100, 'X':10,'V':5,'I':1}
+    resultInteger=0
+    for i in range(0,len(romanInput)-1):
+        if roman[romanInput[i]]<roman[romanInput[i+1]]:
+            resultInteger-=roman[romanInput[i]]
+        else:
+            resultInteger+=roman[romanInput[i]]
+    return resultInteger+roman[romanInput[-1]]
+
